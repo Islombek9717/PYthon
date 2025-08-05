895bb02c2c1f9a9bc5af50d0333b48d3195383e9
Islombek <sultonovislom9717@gmail.com>
2025-08-05 17:33:30 +1000

    Python Lab2 second question

--- app.py
+++ app.py
@@ -11,3 +11,21 @@ my_koala = Koala("Koko", 3)
 
 
 my_koala.greet()
+
+
+class Bird:
+    def make_soud(self):
+        print("Tweet!")
+
+
+class Emu(Bird):
+    def make_soud(self):
+        print("Ground!")
+
+
+def speak(bird):
+    bird.make_soud()
+
+
+speak(Bird())
+speak(Emu())
